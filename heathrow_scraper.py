46cab773c0b52943af44ff46843b02784c782bba
JureStabuc <jure.stabuc@gmail.com>
2017-03-16 15:09:48 +0000

    fix

--- heathrow_scraper.py
+++ heathrow_scraper.py
@@ -25,33 +25,54 @@ with open("heathrow_data.csv", "w") as heathrow_data:
     writer.writeheader()
 
     for page in data_links:
-        final_data = []
+        #final_data = []
         data_page = requests.get(page).text
-        print(page)
+        #print(page)
         soup = BS(data_page, "html.parser")
         date = soup.find("title")
         table = soup.find("tbody")
         rows = table.find_all("tr", class_=["row-3", "row-4", "row-36", "row-37"])
-        for row in rows:
-            data = {
-                "date" : [],
-                "arrivals" : [],
-                "departures" : [],
-                "complaints" : [],
-                "total complaints" : [],
-            }
-
-            cols = [row.find_all("td", class_="column-2")[0] for row in rows]
-            data["arrivals"].append(cols[0].get_text())
-            data["departures"].append(cols[1].get_text())
-            data["complaints"].append(cols[2].get_text())
-            data["total complaints"].append(cols[3].get_text())
-
+        data = {
+            "date" : [],
+            "arrivals" : [],
+            "departures" : [],
+            "complaints" : [],
+            "total complaints" : [],
+        }
         for day in date:
             data["date"].append(day)
-            final_data.append(data)
-        time.sleep(1)
 
-        row = {"date": day, "total num arrivals": data["arrivals"][0], "total num departures": data["departures"][0], "total num of people making complaints": data["complaints"][0], "total number of all complaints (phone, email, web, letters)": data["total complaints"][0] }
-        
-        writer.writerow(row)
+        if "2016" or "2015" in day:
+            notes = [row.find_all("td", class_="column-1")[0] for row in rows]
+            for note in notes:
+                note_text = note.get_text()
+
+            cols = [row.find_all("td", class_="column-2")[0] for row in rows]
+
+            if len(cols)==4:
+                if "Notes" in note_text:
+                    data["arrivals"].append(cols[0].get_text())
+                    data["departures"].append(cols[1].get_text())
+                    #data["complaints"].append(cols[3].get_text())
+                    data["total complaints"].append(cols[2].get_text())
+                    row = {"date": day, "total num arrivals": data["arrivals"][0], "total num departures": data["departures"][0], "total number of all complaints (phone, email, web, letters)": data["total complaints"][0] }
+
+                else:
+                    data["arrivals"].append(cols[0].get_text())
+                    data["departures"].append(cols[1].get_text())
+                    data["complaints"].append(cols[2].get_text())
+                    data["total complaints"].append(cols[3].get_text())
+                    row = {"date": day, "total num arrivals": data["arrivals"][0], "total num departures": data["departures"][0], "total num of people making complaints": data["complaints"][0], "total number of all complaints (phone, email, web, letters)": data["total complaints"][0] }
+            else:
+                data["arrivals"].append(cols[0].get_text())
+                data["departures"].append(cols[1].get_text())
+                data["total complaints"].append(cols[2].get_text())
+                row = {"date": day, "total num arrivals": data["arrivals"][0], "total num departures": data["departures"][0], "total number of all complaints (phone, email, web, letters)": data["total complaints"][0] }
+
+
+            time.sleep(1)
+            writer.writerow(row)
+            print("{} - ok".format(page))
+        else:
+            print("END".format(page))
+            break
